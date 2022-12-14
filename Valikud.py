2d6bad637e9146bab83c3967cdc48c755b086216
opilane <opilane@HP-CZC6297B8P>
2022-12-14 14:33:00 +0200

    Lisa ülesaned

--- Valikud.py
+++ Valikud.py
@@ -1,42 +1,101 @@
-# 1 Ülesane
+#
+from math import *
+    
+
+from math import *
+print("Sisseloginine tahvel")
+try:
+    vanus=int(input("Kui vana sa oled? "))
+    if vanus>=18:
+        print("Kas te annate vanematele loa oma Tahvelit vaadata? ")
+        o=(input("Jah või ei. "))
+        if o.lower()=="jah": #upper() muutaja  sõnad surem
+            print({o})
+            print("See on ligipääs teie vanematele.")
+            print("Tahvel on kini. ")
+        elif o.upper()=="Ei":
+            print("Siesepääs puudub. ")
+            print("Tahvel on kinni")
+    if 18>vanus:
+        print("Juurdepääs vanematele on automaatselt antud. ")
+except:
+    print("Tahvel on kinni.")
+print()
+
+
+
+
+# minu ülesane 2
 from math import *
 print("Arvutage sinu normale pikkus.")
 k=int(input("Mis on sinu kaal: "))
 h=int(input("Mis on sinu kasv: "))
 v=int(input("Mis on sinu vanus: "))
-if h>0:
-    n=h+10
-    n2=h+10
-    if n>n2: 
-        print(f"Normale kasv {n}.")
-    elif n2>n: 
-        print(f"Normale kasv {n2}.")
-    else:
-        print(f"Sinu normale kasv on {n} cm.")
-print()
+try:
+    if h>0:
+        n=h+10
+    if n: 
+        print(f"Sinu normale kasv {n} cm.")
+except:
+    print("Vale")
 
 
-# 2 Ülesane
+
+
+
+
+# minu ülesane 1
 try:
     p=int(input("Mis päev täna on "))
 except:
     print("Vale")
 if  p==1:
-    print("Täna on 6 tundid.")
+    print("Täna on 6 tundi.")
 elif p==2:
-    print("Täna on 4 tundid.")
+    print("Täna on 4 tundi.")
 elif p==3:
-    print("Täna on 9 tundid.")
+    print("Täna on 9 tundi.")
 elif p==4:
-    print("Täna on 6 tundid.")
+    print("Täna on 6 tundi.")
 elif p==5:
-    print("Täna on 7 tundid.")
+    print("Täna on 7 tundi.")
 elif p==6:
-    print("Täna ei ole tundid, puhkate.")
+    print("Täna ei ole tundi, puhkate.")
 elif p==7:
-    print("Täna on vimane päev kuni uus töönädala")
+    print("Täna on 0 tundi. ")
 print()
-    
+
+#
+try:
+    päev=int(input("Mis päev ja mitu tundi täana on ?"))
+    if päev==1:
+        n="Esmaspäev"
+        n="6 tundi"
+    elif päev==2:
+        n="Teisepäev"
+        n="8 tundi"
+    elif päev==3:
+        n="Kolmapäev"
+        n="6 tundi"
+    elif päev==4:
+        n="Neljapäev"
+        n="5 tundi"
+    elif päev==5:
+        n="Reede"
+        n="7 tundi"
+    elif päev==6:
+        n="Laupäev"
+        n="0 tundi"
+    elif päev==7:
+        n="Pühapäev"
+        n=" 0 tundi"
+    else:
+        n="vale number"
+    print(n) 
+except:
+    print("!!!")
+
+
 
 #13.12.22
 from random import *
